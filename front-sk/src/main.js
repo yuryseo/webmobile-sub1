00c909ffe252fd47cb6960a29162a67782b3f97d
yury <yuryseo@users.noreply.github.com>
2020-01-14 17:54:58 +0900

    history  mode update
     history  mode를 추가 해야 http://localhost:8080/components 를 볼 수 있게 됨

--- front-sk/src/main.js
+++ front-sk/src/main.js
@@ -8,7 +8,10 @@ Vue.config.productionTip = false
 
 Vue.use(Router)
 
+// history mode 가 뭔지 찾아보기
+// history  mode를 추가 해야 http://localhost:8080/components 를 볼 수 있어
 const router = new Router({
+    mode : "history",
     routes,
 });
 
