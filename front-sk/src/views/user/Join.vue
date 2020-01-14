deb1abebcd1460b087e53e1c65942c726e46c507
yury <yuryseo@users.noreply.github.com>
2020-01-14 17:56:00 +0900

    Join Update
    Join 화면 수정

--- front-sk/src/views/user/Join.vue
+++ front-sk/src/views/user/Join.vue
@@ -5,60 +5,73 @@
     Sub PJT I에서는 UX, 디자인 등을 포함하여 백엔드를 제외하여 개발합니다.
  -->
 <template>
-    <div>
-        <h1>가입하기</h1>
-        <div>
-            <div>
-                <input v-model="nickName"
-                       id="nickname"
-                       placeholder="닉네임을 입력하세요." type="text"/>
-                <label for="nickname">닉네임</label>
+    <div  class="user" >    
+        <!-- 여기 class 추가한거  --> 
+        <div class="wrapC">
+            <h1>가입하기</h1>
+            <div class="join">
+                <div class="input-with-label">
+                    <input v-model="nickName"
+                        id="nickname" 
+                        placeholder="닉네임을 입력하세요." type="text"/>
+                    <label for="nickname">닉네임</label>
+                </div>
+
+                <div class="input-with-label">
+                    <input v-model="email"
+                        id="email" placeholder="이메일을 입력하세요."
+                        type="text"/>
+                    <label for="email">이메일</label>
+
+                </div >
+                
+                
+                <div class="input-with-label">
+                    <input v-model="phone"
+                        id="phone" type="text"
+                        placeholder="핸드폰 번호를 입력하세요."/>
+                    <label for="phone">핸드폰 번호</label>
+                </div>
+
+                <div class="input-with-label">
+                    <input v-model="password"
+                        id="password" :type="passwordType"
+                        placeholder="비밀번호를 입력하세요."/>
+                    <label for="password">비밀번호</label>
+                </div>
+
+                <div class="input-with-label">
+                    <input v-model="passwordConfirm" :type="passwordConfirmType"
+                            id="password-confirm"
+                        placeholder="비밀번호를 다시한번 입력하세요."/>
+                    <label for="password-confirm">비밀번호 확인</label>
+                </div>
             </div>
 
-            <div>
-                <input v-model="email"
-                       id="email" placeholder="이메일을 입력하세요."
-                       type="text"/>
-                <label for="email">이메일</label>
+            <label>
+                <input v-model="isTerm" type="checkbox"  id="term"/>
+                <span>약관을 동의합니다.</span>
+            </label>
 
-            </div>
-
-            <div>
-                <input v-model="password"
-                       id="password" :type="passwordType"
-                       placeholder="비밀번호를 입력하세요."/>
-                <label for="password">비밀번호</label>
-            </div>
-
-            <div>
-                <input v-model="passwordConfirm" :type="passwordConfirmType"
-                        id="password-confirm"
-                       placeholder="비밀번호를 다시한번 입력하세요."/>
-                <label for="password-confirm">비밀번호 확인</label>
+            <span @click="termPopup=true" class="btn--text" >약관보기</span>
+            <div class="btn-bottom">
+                <button>
+                    가입하기
+                </button>
             </div>
         </div>
-
-        <label>
-            <input v-model="isTerm" type="checkbox" id="term"/>
-            <span>약관을 동의합니다.</span>
-        </label>
-
-        <span @click="termPopup=true">약관보기</span>
-
-        <button>
-            가입하기
-        </button>
-
-
     </div>
 </template>
 
 <script>
+    import '../../assets/css/style.scss'
+    import '../../assets/css/user.scss'
 
     export default {
         data: () => {
             return {
                 email: '',
+                phone: '',
                 password: '',
                 passwordConfirm: '',
                 nickName: '',
@@ -66,6 +79,7 @@
                 isLoading: false,
                 error: {
                     email: false,
+                    phone: false,
                     password: false,
                     nickName: false,
                     passwordConfirm: false,
