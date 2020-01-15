3e7e888baddb5ffecb9a27c4541e17e7f90ff4b8
yury <yuryseo@users.noreply.github.com>
2020-01-15 17:56:28 +0900

    Update FindPassword.vue
    ux/ui 고려 업데이트

--- front-sk/src/views/user/FindPassword.vue
+++ front-sk/src/views/user/FindPassword.vue
@@ -1,37 +1,52 @@
 <template>
-    <div  class="user" >    
-        <!-- 여기 class 추가한거  --> 
+    <div class="user">
+        <!-- 여기 class 추가한거 -->
         <div class="wrapC">
             <h1>비밀번호 찾기</h1>
             <div>
                 <div class="input-with-label">
-                    <input v-model="id"
-                        id="id" 
-                        placeholder="아이디를 입력하세요." type="text"/>
+                    <input v-model="id" id="id" placeholder="아이디를 입력하세요." type="text"/>
                     <label for="id">아이디</label>
-                </div>
-
-                <div class="input-with-label">
-                    <input v-model="email"
-                        id="email" placeholder="이메일을 입력하세요."
-                        type="text"/>
-                    <label for="email">이메일</label>
-
-                </div >
-
-                <div class="input-with-label">
-                    <input v-model="phone"
-                        id="phone" type="text"
-                        placeholder="핸드폰 번호를 입력하세요."/>
-                    <label for="phone">핸드폰 번호</label>
+                     <br/> <br/> <br/> <br/> <br/>
                 </div>
             </div>
 
-            <div class="btn-bottom">
-                <button>
-                    비밀번호 찾기
-                </button>
+            <div class="add-option">
+
+                <div>
+
+                    <div class="wrap">
+                        <router-link  v-bind:to="{name:'ChangePassword'}" >
+                        <button class="btn btn--back btn--login">이메일로 임시 비밀번호 받기</button></router-link>
+                        <br/> <br/> <br/>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="add-option">
+
+                <div>
+
+                    <div class="wrap">
+                        <router-link  v-bind:to="{name:'ChangePassword'}" >
+                        <button class="btn btn--back btn--login">핸드폰으로 임시 비밀번호 받기</button></router-link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+
+
+<script>
+
+    import '../../assets/css/style.scss'
+    import '../../assets/css/user.scss'
+
+    export default {
+        name: "FindPassword"
+    }
+
+</script>
