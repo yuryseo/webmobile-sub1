58e28c161b27e90eef08e1431a9ceb7e3da3d21c
yury <yuryseo@users.noreply.github.com>
2020-01-16 17:56:36 +0900

    axios, qs 추가
    axios, qs 추가

--- front-sk/src/apis/UserApi.js
+++ front-sk/src/apis/UserApi.js
@@ -1,12 +1,21 @@
+import Axios from "axios"
+import qs from 'qs';
+
 /*
  User API 예시
  */
 
 const requestLogin = (data,callback,errorCallback) => {
     //백앤드와 로그인 통신하는 부분
-
+Axios.post("http://localhost:8080/account/login",qs.stringify(data))
+.then(Response=>{
+    callback(Response);
+}).catch(Error=>{
+    errorCallback(Error);
+})
 
 }
+
 
 const UserApi = {
     requestLogin:(data,callback,errorCallback)=>requestLogin(data,callback,errorCallback)
